a13e9ce1457c8d232f490dcd0e37b2fc9a152c8c
M. Iqbal Effendi <iqbaleff214@gmail.com>
2022-12-18 17:17:20 +0800

    go: change struct to map for flexibility

--- go/main.go
+++ go/main.go
@@ -8,33 +8,8 @@ import (
 )
 
 type Dataset struct {
-	Criteria     []*Criterion
-	Alternatives []*Alternative
-}
-
-type Criterion struct {
-	Code             string  `json:"code"`
-	Name             string  `json:"name"`
-	Type             string  `json:"type"`
-	Weight           float64 `json:"weight"`
-	NormalizedWeight float64
-	DivisorValue     float64
-}
-
-type Alternative struct {
-	ID           int     `json:"id"`
-	Name         string  `json:"name"`
-	C1           float64 `json:"C1"`
-	NormalizedC1 float64
-	C2           float64 `json:"C2"`
-	NormalizedC2 float64
-	C3           float64 `json:"C3"`
-	NormalizedC3 float64
-	C4           float64 `json:"C4"`
-	NormalizedC4 float64
-	C5           float64 `json:"C5"`
-	NormalizedC5 float64
-	Result       float64
+	Criteria     []map[string]any
+	Alternatives []map[string]any
 }
 
 func main() {
@@ -63,7 +38,7 @@ func main() {
 
 	// mengurutkan hasil berdasarkan pembobotan matriks tertinggi
 	sort.Slice(dataset.Alternatives, func(i, j int) bool {
-		return dataset.Alternatives[i].Result > dataset.Alternatives[j].Result
+		return dataset.Alternatives[i]["result"].(float64) > dataset.Alternatives[j]["result"].(float64)
 	})
 
 	// menampilkan hasil menggunakan std out CLI
@@ -71,7 +46,7 @@ func main() {
 	fmt.Println("No.\tNama \t\tHasil")
 	fmt.Println("================================")
 	for i, a := range dataset.Alternatives {
-		fmt.Printf("%2d\t%-10s \t %.2f\n", i+1, a.Name, a.Result)
+		fmt.Printf("%2d\t%-10s \t %.2f\n", i+1, a["name"], a["result"])
 	}
 }
 
@@ -80,12 +55,12 @@ func (dataset *Dataset) weightNormalization() {
 	// Menghitung total bobot
 	var total float64
 	for _, c := range dataset.Criteria {
-		total += c.Weight
+		total += c["weight"].(float64)
 	}
 
 	// Menghitung hasil normalisasi masing-masing bobot
 	for _, c := range dataset.Criteria {
-		c.NormalizedWeight = c.Weight / total
+		c["normalizedWeight"] = c["weight"].(float64) / total
 	}
 }
 
@@ -94,36 +69,17 @@ func (dataset *Dataset) matrixNormalization() {
 	// menentukan nilai pembagi untuk masing-masing kriteria berdasarkan nilai-nilai alternatif
 	for _, a := range dataset.Alternatives {
 		for _, c := range dataset.Criteria {
-			switch c.Code {
-			case "C1":
-				c.DivisorValue = divisorValue(a.C1, c.DivisorValue, c.Type)
-			case "C2":
-				c.DivisorValue = divisorValue(a.C2, c.DivisorValue, c.Type)
-			case "C3":
-				c.DivisorValue = divisorValue(a.C3, c.DivisorValue, c.Type)
-			case "C4":
-				c.DivisorValue = divisorValue(a.C4, c.DivisorValue, c.Type)
-			case "C5":
-				c.DivisorValue = divisorValue(a.C5, c.DivisorValue, c.Type)
+			if c["divisorValue"] == nil {
+				c["divisorValue"] = 0.0
 			}
+			c["divisorValue"] = divisorValue(a[c["code"].(string)].(float64), c["divisorValue"].(float64), c["type"].(string))
 		}
 	}
 
 	// normalisasi matriks berdasarkan nilai pembaginya
 	for _, a := range dataset.Alternatives {
 		for _, c := range dataset.Criteria {
-			switch c.Code {
-			case "C1":
-				a.NormalizedC1 = normalize(a.C1, c.DivisorValue, c.Type)
-			case "C2":
-				a.NormalizedC2 = normalize(a.C2, c.DivisorValue, c.Type)
-			case "C3":
-				a.NormalizedC3 = normalize(a.C3, c.DivisorValue, c.Type)
-			case "C4":
-				a.NormalizedC4 = normalize(a.C4, c.DivisorValue, c.Type)
-			case "C5":
-				a.NormalizedC5 = normalize(a.C5, c.DivisorValue, c.Type)
-			}
+			a["normalized"+c["code"].(string)] = normalize(a[c["code"].(string)].(float64), c["divisorValue"].(float64), c["type"].(string))
 		}
 	}
 }
@@ -131,23 +87,15 @@ func (dataset *Dataset) matrixNormalization() {
 // method untuk menghitung pembobotan matriks yang telah dinormalisasi
 func (dataset *Dataset) resultCalculation() {
 	for _, a := range dataset.Alternatives {
+		var result float64
 		for _, c := range dataset.Criteria {
-			switch c.Code {
-			case "C1":
-				a.Result += (a.NormalizedC1 * c.NormalizedWeight)
-			case "C2":
-				a.Result += (a.NormalizedC2 * c.NormalizedWeight)
-			case "C3":
-				a.Result += (a.NormalizedC3 * c.NormalizedWeight)
-			case "C4":
-				a.Result += (a.NormalizedC4 * c.NormalizedWeight)
-			case "C5":
-				a.Result += (a.NormalizedC5 * c.NormalizedWeight)
-			}
+			result += (a["normalized"+c["code"].(string)].(float64) * c["normalizedWeight"].(float64))
 		}
+		a["result"] = result
 	}
 }
 
+// function untuk menghitung nilai normalisasi matriks
 func normalize(matrix, divisor float64, criteriaType string) float64 {
 	if criteriaType == "cost" {
 		return divisor / matrix
@@ -156,6 +104,7 @@ func normalize(matrix, divisor float64, criteriaType string) float64 {
 	return matrix / divisor
 }
 
+// function untuk mendapatkan pembagi yang akan digunakan untuk normalisasi matriks
 func divisorValue(value, initial float64, criteriaType string) float64 {
 	if criteriaType == "cost" && (value < initial || initial == 0) {
 		return value
